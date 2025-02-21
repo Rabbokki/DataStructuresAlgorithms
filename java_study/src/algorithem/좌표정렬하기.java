8fcd24c129dc81d271019d62f763e82a010ce4e3
Rabbokki <j552607035@gmail.com>
2025-02-21 20:59:30 +0900

    test1

--- java_study/src/algorithem/좌표정렬하기.java
+++ java_study/src/algorithem/좌표정렬하기.java
@@ -6,5 +6,10 @@ public class 좌표정렬하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("bbbb");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
     }
 }
